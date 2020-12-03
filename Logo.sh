83c3b9ad474f83b626764af2480c7fea7f168199
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-12-04 01:21:05 +0500

    Create Logo.sh

---
+++ Logo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#clolors
+white='\e[1;37m'
+green='\e[0;32m'
+blue='\e[1;34m'
+red='\e[1;31m'
+yellow='\e[1;33m' 
+echo ""
+echo""
+banner() {
+ echo -e $'\e[1;33m\e[0m\e[1;31m«-----------------Disclaimer---------------»\e[0m''\e[1;33m\e[0m\e[1;32m  \e[0m'
+   echo -e $'\e[1;37m\e[0m\e[1;31m  This Tool is for Educational Purpose\e[0m'
+      echo ""                                                
+      }
+      banner
