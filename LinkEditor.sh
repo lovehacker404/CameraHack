4b6f576af443a63cfcead84b186aba468dd042f1
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-12-07 05:25:24 +0500

    Update LinkEditor.sh

--- LinkEditor.sh
+++ LinkEditor.sh
@@ -28,16 +28,18 @@ read phish
 url_checker $phish
 short=$(curl -s https://da.gd/s/?url=${phish})
 shorter=${short#https://}
-echo -e "\n\e[1;32;42m           Making Domain            \e[0m"
-echo 'Domain to Make the Phishing URL (with http or https), ex https://google.com, http
-://anything.org) :'
+echo -e "\n\e[1;32;42m           Making Domain              \e[0m"
+echo -e "\e[31m Domain to Make the Phishing URL (with http or https)\e[0m"
+echo -e "\e[32m https://google.com\e[0m"
+echo -e "\e[33m http://anything.org\e[0m"
+echo -e "\e[34m https://facebook.com\e[0m"
 echo -en "\e[32m=>\e[0m "
 read mask
 url_checker $mask
-echo -e '\n\e[1;33;42mType social engineering words:(like free-money, best-pubg-tricks)'
+echo -e '\n\e[1;33;42mType social engineering words:(like free-money, best-pubg-tricks)\e[0m'
 echo -e "\e[31mDon't use space just use '-' between social engineering words\e[0m"
 echo -en "\e[32m==>\e[35m "
 read words
-echo -e "\n\e[1;34;42m        Generating Phish Link...       \n"
+echo -e "\n\e[1;34;42m        Generating Phish Link...       \n\e[0m"
 final=$mask-$words@$shorter
-echo -e "\e[33mHere is the Phish URL:\e[35m ${final} \e[0m\n"
+echo -e "\e[33mHere is the Phish URL:\e[36m ${final} \e[0m\n"
